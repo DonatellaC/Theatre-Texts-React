b06361c6559ae544aeaa2c7621637b5c511abb98
DonatellaC <donatella.carta@mailbox.org>
2021-01-14 22:18:07 +0100

    style: install and use styled-components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import React from 'react';
+import GlobalStyle from './globalStyles.js';
 import Line from "./components/Line";
-import './App.css';
+import styled from "styled-components";
+
+const Title = styled.h1`
+  text-align: center;
+  color: white;
+  font-size: 4rem;
+  padding: 35px 20px 0 20px;
+  margin: -5px;
+`;
+
+const BackgImg = styled.div`
+  background-image: url("https://images.unsplash.com/photo-1593964269618-29948d5e2a29?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80");
+  height: 750px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  @media (max-width: 320px) { 
+    height: 950px;
+  }
+
+  @media (min-width: 768px) and (max-width: 820px){ 
+    height: 1050px;
+  }
+
+  @media (min-width: 1024px) and (max-width: 1030px) { 
+    height: 1400px;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <Line />
-      </header>
-    </div>
+    <BackgImg>
+      <GlobalStyle />
+      <div>
+        <Title>ROMEO &amp; JULIET</Title>
+      </div>
+      <div>
+        <Line />
+      </div>
+    
+    </BackgImg>
   );
 }
 
--- src/components/Line.js
+++ src/components/Line.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import text from "./text.json";
+import styled from "styled-components";
+
+const ListTheatre = styled.li`
+  color: white;
+  padding-top: 10px;
+  text-align: justify;
+`;
+
+const ListStyle = styled.ul`
+list-style-type: none;
+padding: 60px 20px 30px 20px;
+font-size: 1.5rem;
+letter-spacing: 1px;
+
+ @media (min-width: 768px) and (max-width: 820px){ 
+    font-size: 2rem;
+  }
+
+ @media (min-width: 1024px) and (max-width: 1030px) { 
+    font-size: 2.5rem;
+  }
+
+`;
+
 
 const theatreText = text;
 
@@ -7,20 +31,20 @@ function Line() {
 
     return (
         <div>
-            <ul>
+            <ListStyle>
                 {/* Map - json file */}
                 {
                     theatreText.map((text, i) => {
                         return(
                             <div key={i}>
-                                <li>{text}</li>
+                                <ListTheatre>{text}</ListTheatre>
                             </div>
                         )
                     }
                 )}
-            </ul>
+            </ListStyle>
         </div>
     )
 }
 
-export default Line
+export default Line;
